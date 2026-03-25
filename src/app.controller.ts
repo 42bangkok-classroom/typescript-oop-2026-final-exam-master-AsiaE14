bd3b4d35c7c73bfb5e830168ae8f2653fe18b254
AsiaE14 <sadgudo@gmail.com>
2026-03-25 10:16:08 +0700

    a

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,12 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello() {
+    const data = this.appService.getHello();
+    return {
+      success:true,
+      data:data,
+      message: "Hello NestJS"
+    };
   }
 }
